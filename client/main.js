b89fa8e74dab77024da50de5023011821025d4d5
vjblind <vjblind@gmail.com>
2017-03-30 01:22:57 +0200

    up

--- client/main.js
+++ client/main.js
@@ -1,7 +1,16 @@
 import { Template } from 'meteor/templating';import './main.html';
 
+var todos= [{ text: "test", done:true},{text: "test",done: true  },{text: "test",done:false  },{text: "test"  }
+];
+
 Template.todolist.helpers({
-  todos: function(){  return [{ text: "test", done:true},{text: "test",done: true  },{text: "test",done:false  },{text: "test"  }
-]}
+  todos: function(){  return todos;}
  ,
+});
+
+
+Template.todolist.events({
+  "click #button": function(){
+todo.push({ text: "test"          });
+  }
 });
